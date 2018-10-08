8f02c0a800bb22c3934185d92a85ca8a2ae3fc59
Aleksandra Półtorak <xmacabrax@gmail.com>
2018-10-09 01:05:58 +0200

    Dodanie .gitignore

--- lista-zakupow/ctrl.js
+++ lista-zakupow/ctrl.js
@@ -2,9 +2,9 @@ app.controller('myCtrl', function ($scope) {
     $scope.appTitle = "Lista zakupów";
     $scope.productName = ["Mleko", "Jajka", "Chleb"]
     $scope.products = [
-        {name: "Mleko", amount: "1"},
-        {name: "Jajka", amount: "6"},
-        {name: "Chleb", amount: "1"}];
+        {name: "Mleko", amount: 1},
+        {name: "Jajka", amount: 6},
+        {name: "Chleb", amount: 1}];
     $scope.addAm = 1;
     $scope.addProduct = function () {
         $scope.errorText = "";
@@ -40,6 +40,11 @@ app.controller('myCtrl', function ($scope) {
     $scope.decreaseAmount = function (i) {
         if ($scope.products[i].amount > 1) {
             $scope.products[i].amount--;
+        }
+    }
+$scope.updateAmount = function (i) {
+        if ($scope.products[i].amount !== this.value) {
+            $scope.products[i].amount = this.value;
         }
     }
 
